4466b81d79b9eb0903a8d5d078f09280e07c250b
Korchea <6334@holbertonstudents.com>
2023-08-04 15:55:50 -0700

    Task 10 completed to check

--- models/place.py
+++ models/place.py
@@ -10,7 +10,7 @@ place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60), ForeignKey(
                           'place.id'), primary_key=True, nullable=False),
                       Column('amenity_id', String(60), ForeignKey(
-                          'amenity.id'), primary_key=True, nullable=False)
+                          'amenities.id'), primary_key=True, nullable=False)
                       )
 
 
