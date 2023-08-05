852e01e1f2e191d86545908c56059984bef9e056
Patricio-Benglian <6305@holbertonstudents.com>
2023-08-05 11:20:46 -0700

    place.id -> places.id

--- models/place.py
+++ models/place.py
@@ -8,7 +8,7 @@ from os import getenv
 
 place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60), ForeignKey(
-                          'place.id'), primary_key=True, nullable=False),
+                          'places.id'), primary_key=True, nullable=False),
                       Column('amenity_id', String(60), ForeignKey(
                           'amenities.id'), primary_key=True, nullable=False)
                       )
