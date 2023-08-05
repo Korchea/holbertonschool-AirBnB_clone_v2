1af26a1e2982cea1b16aecc89060147dcf33bef7
Patricio-Benglian <6305@holbertonstudents.com>
2023-08-05 13:30:33 -0700

    Fixed place_amenity typos. Fixed some relation and key typos

--- models/place.py
+++ models/place.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 from os import getenv
 
-
 place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60), ForeignKey(
                           'places.id'), primary_key=True, nullable=False),
@@ -17,25 +16,38 @@ place_amenity = Table('place_amenity', Base.metadata,
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey(
-        "cities.id"), nullable=False)  # ""
-    user_id = Column(String(60), ForeignKey(
-        "users.id"), nullable=False)  # ""
-    name = Column(String(128), nullable=False)  # ""
-    description = Column(String(1024), nullable=True)  # ""
-    number_rooms = Column(Integer, default=0, nullable=False)  # 0
-    number_bathrooms = Column(Integer, default=0, nullable=False)  # 0
-    max_guest = Column(Integer, default=0, nullable=False)  # 0
-    price_by_night = Column(Integer, default=0, nullable=False)  # 0
-    latitude = Column(Float)  # 0.0
-    longitude = Column(Float)  # 0.0
-    amenity_ids = []  # No longer in use in db
-    if getenv("HBNB_TYPE_STORAGE") == "db":
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        city_id = Column(String(60), ForeignKey(
+            "cities.id"), nullable=False)  # ""
+        user_id = Column(String(60), ForeignKey(
+            "users.id"), nullable=False)  # ""
+        name = Column(String(128), nullable=False)  # ""
+        description = Column(String(1024), nullable=True)  # ""
+        number_rooms = Column(Integer, default=0, nullable=False)  # 0
+        number_bathrooms = Column(Integer, default=0, nullable=False)  # 0
+        max_guest = Column(Integer, default=0, nullable=False)  # 0
+        price_by_night = Column(Integer, default=0, nullable=False)  # 0
+        latitude = Column(Float)  # 0.0
+        longitude = Column(Float)  # 0.0
+        amenity_ids = []  # No longer in use in db
         reviews = relationship(
-            "Review", cascade='all, delete, delete-orphan', backref="place")
+            "Review", cascade='all, delete, delete-orphan', backref="places")
         amenities = relationship(
-            "Amenity", secondary=place_amenity, viewonly=False, back_populates="places_amenities")
+            "Amenity", secondary=place_amenity, viewonly=False, back_populates="place_amenity")
     else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
+
         @property
         def reviews(self):
             """"Returns cities that share state.id"""
--- models/state.py
+++ models/state.py
@@ -11,11 +11,13 @@ from os import getenv
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state",
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="states",
                               cascade="all, delete, delete-orphan")
     else:
+        name = ""
+
         @property
         def cities(self):
             """Returns cities that share state.id"""
