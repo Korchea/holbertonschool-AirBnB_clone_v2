a146df9429f6cc370de42e4a1f81c11144a357a5
Korchea <6334@holbertonstudents.com>
2023-08-15 06:59:34 -0700

    Task 4 completed

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application
+
+    Returns:
+        Str: Hello HBNB
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def say_Hello():
+    """Return a message when starts"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def say_HBNB():
+    """Return a message"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def say_C_text(text):
+    """Return a message"""
+    txt = text.replace('_', ' ')
+    return "C {}".format(txt)
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def say_python_text(text="is cool"):
+    """Return a message"""
+    txt = text.replace('_', ' ')
+    return "Python {}".format(txt)
+
+
+@app.route("/number/<int:n>")
+def say_python_text(n):
+    """Return a message"""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
