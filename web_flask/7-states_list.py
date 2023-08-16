b9a3736d0dd98c305a8a0fcd22786fb9fd6d8171
Korchea <6334@holbertonstudents.com>
2023-08-16 06:49:04 -0700

    Task 8 to check

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,11 +13,13 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown():
+    """This def terdadown the database"""
     storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
 def say_list():
+    """Return a HTML page with the lists of States"""
     state = storage.all(State)
     sort_dict = dict(sorted(state.items(), key=lambda item: item[1]))
     return render_template("7-states_list.html", tables="States", states=sort_dict)
