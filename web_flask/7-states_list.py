9e792ed618ac31a343a374fb4acf26e1bb8875b6
Korchea <6334@holbertonstudents.com>
2023-08-16 06:43:01 -0700

    Task 8 completed

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application
+
+    Returns:
+        Str: Hello HBNB
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown():
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def say_list():
+    state = storage.all(State)
+    sort_dict = dict(sorted(state.items(), key=lambda item: item[1]))
+    return render_template("7-states_list.html", tables="States", states=sort_dict)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
