3bd9f857aea65a56515af8ab60ae69058a683fb4
Korchea <6334@holbertonstudents.com>
2023-08-16 09:23:47 -0700

    Task 8 completed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,8 +5,6 @@
         Str: Hello HBNB
 """
 from flask import Flask, render_template
-from models import storage
-from models.state import State
 
 app = Flask(__name__)
 
@@ -14,24 +12,19 @@ app = Flask(__name__)
 @app.teardown_appcontext
 def teardown(exept):
     """This def terdadown the database"""
+    from models import storage
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Return a HTML page with the lists of States"""
-    states = storage.all(State)
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
-
-
-"""@app.route("/states_list", strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def say_list():
-    
+    """Return a HTML page with the lists of States"""
+    from models.state import State
+    from models import storage
     all_state = storage.all(State)
     sort_dict = sorted(all_state.values(), key=lambda state: state.name)
     return render_template("7-states_list.html", state=sort_dict)
-"""
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
