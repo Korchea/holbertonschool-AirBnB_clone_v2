f37fc3ece7cf7051ce0023529ca87cdb37fa2b3e
Korchea <6334@holbertonstudents.com>
2023-08-16 09:20:19 -0700

    Task 8 completed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -17,13 +17,21 @@ def teardown(exept):
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def say_list():
+@app.route('/states_list', strict_slashes=False)
+def states_list():
     """Return a HTML page with the lists of States"""
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+"""@app.route("/states_list", strict_slashes=False)
+def say_list():
+    
     all_state = storage.all(State)
     sort_dict = sorted(all_state.values(), key=lambda state: state.name)
     return render_template("7-states_list.html", state=sort_dict)
-
+"""
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
